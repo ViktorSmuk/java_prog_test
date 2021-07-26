94da9276f0b68897c57a0db60d812356c55b8bad
ViktorSmuk <smuk_92@mail.ru>
2021-07-26 21:59:10 +0300

    Оставляем дефолтные значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("TEST2", "TEST3", "TEST1"));
+    app.getGroupHelper().fillGroupForm(new GroupData("TEST2", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
