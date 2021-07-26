2d539a83f949c96d3bb3698058bb0d680cb41d9b
ViktorSmuk <smuk_92@mail.ru>
2021-07-26 21:49:20 +0300

    Реализован запуск тестов в разных браузерах

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquetionTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquetionTests.java
@@ -23,4 +23,21 @@ public class EquetionTests {
     Assert.assertEquals(e.rootNumber(),2);
   }
 
+  @Test
+  public void testLinear(){
+    Equetion e = new Equetion(0,1,1);
+    Assert.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void testConstant(){
+    Equetion e = new Equetion(0,0,1);
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void testZero(){
+    Equetion e = new Equetion(0,0,0);
+    Assert.assertEquals(e.rootNumber(),-1);
+  }
 }
